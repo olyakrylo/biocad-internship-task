5c0097f805d5e3b5503a64983065753ab6779444
olyakrylo <olyakrylo24@gmail.com>
2020-03-30 18:59:38 +0300

    input -> select in search

--- layout/src/components/Search/Search.js
+++ layout/src/components/Search/Search.js
@@ -6,8 +6,12 @@ export default class Header extends React.Component {
         return (
             <div className='search'>
                 <p className='search__title'>Enter inventory number, guid or bims id</p>
-                <form className='search__form'>
-                    <input placeholder='A-001234' className='search__input'></input>
+                <form className='search__form' defaultValue='2'>
+                    <select className='search__select'>
+                        <option value='1'>A-001233</option>
+                        <option value='2'>A-001234</option>
+                        <option value='3'>A-001235</option>
+                    </select>
                     <button className='search__button'>Search
                         <div className='search__button_hover'></div>
                     </button>
